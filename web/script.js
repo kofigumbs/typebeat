87e0e87f4763ec5160bafefba52dd48779e5d419
Kofi Gumbs <h.kofigumbs@gmail.com>
2020-10-20 00:38:48 -0400

    Key grid

--- web/script.js
+++ web/script.js
@@ -1,1 +1,30 @@
-document.documentElement.innerHTML += '<h2>updated from JS</h2>'
+class GrooveboxKey {
+  constructor(label, index) {
+    this.index = index;
+    this.element = document.createElement("div");
+    this.element.innerText = label;
+    this.element.classList.add("centered", "key");
+  }
+
+  onDown() {
+    this.element.classList.add("down");
+  }
+
+  onUp() {
+    this.element.classList.remove("down");
+  }
+}
+
+
+/*
+ * MAIN
+ */
+
+const keys = {};
+Array.from("qwertyuiopasdfghjkl;zxcvbnm,./").map((label, index) => {
+  const key = new GrooveboxKey(label, index);
+  keys[label] = key;
+  document.body.appendChild(key.element);
+});
+window.addEventListener("keyup", event => keys[event.key] && keys[event.key].onUp())
+window.addEventListener("keydown", event => keys[event.key] && keys[event.key].onDown())
