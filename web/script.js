df8b897404d8c6e5c50ba185b05442a0d1017fcf
Kofi Gumbs <h.kofigumbs@gmail.com>
2020-10-21 23:03:22 -0400

    Hyperscript, Color keyboard by roles

--- web/script.js
+++ web/script.js
@@ -1,42 +1,76 @@
-class GrooveboxKey {
-  constructor(label, index) {
-    this.index = index;
-    this.element = document.createElement("div");
-    this.element.innerText = label;
-    this.element.classList.add("centered", "key");
-  }
+/*
+ * hyperscript helpers
+ */
 
-  onDown() {
-    this.element.classList.add("down");
-  }
-
-  onUp() {
-    this.element.classList.remove("down");
-  }
+const text = string => {
+  return document.createTextNode(string);
 }
+
+const div = (attributes, children) => {
+  const element = document.createElement("div");
+  for (const [ key, value ] of Object.entries(attributes))
+    element.setAttribute(key, value);
+  for (const child of children)
+    element.appendChild(child)
+  return element;
+};
 
 
 /*
- * MAIN
+ * groovebox
  */
+const keys = {
+  layout: {
+    row1: Array.from("qwertyuiop"),
+    row2: Array.from("asdfghjkl;"),
+    row3: Array.from("zxcvbnm,./"),
+  },
+  role: {
+    sequence: Array.from("qwertyuiasdfghjk"),
+    control1: Array.from(".lo"),
+    control2: Array.from("/;p"),
+    navigation: Array.from("zxcvbnm,"),
+  },
+};
 
-const keys = {};
-Array.from("qwertyuiopasdfghjkl;zxcvbnm,./").map((label, index) => {
-  const key = new GrooveboxKey(label, index);
-  keys[label] = key;
-  keys[index] = key;
-  document.body.appendChild(key.element);
-});
-document.addEventListener("keyup", event => keys[event.key] && keys[event.key].onUp());
-document.addEventListener("keydown", event => keys[event.key] && keys[event.key].onDown());
+const newKey = key => {
+  let role;
+  if (keys.role.sequence.includes(key))
+    role = "sequence";
+  if (keys.role.navigation.includes(key))
+    role = "navigation";
+  if (keys.role.control1.includes(key))
+    role = "control one";
+  if (keys.role.control2.includes(key))
+    role = "control two";
+  return div({ class: "key centered " + role, "data-key": key }, [ text(key) ]);
+};
 
-(function loop() {
-  requestAnimationFrame(loop);
-  groovebox().then(({ measure, beat }) => {
-    Object.values(keys).forEach(key => {
-      key.element.classList.remove("beat");
-      key.element.classList.toggle("extra", key.index >= measure);
-    });
-    keys[beat % measure].element.classList.add("beat");
+document.body.appendChild(div({ class: "row centered" }, keys.layout.row1.map(newKey)));
+document.body.appendChild(div({ class: "row centered" }, keys.layout.row2.map(newKey)));
+document.body.appendChild(div({ class: "row centered" }, keys.layout.row3.map(newKey)));
+
+const getKeyElement = key => {
+  return document.querySelector(`[data-key="${key}"]`);
+};
+
+const updateKeyDown = (key, method) => {
+  const element = getKeyElement(key);
+  element && element.classList[method]("down");
+};
+
+document.addEventListener("keydown", event => updateKeyDown(event.key, "add"));
+document.addEventListener("keyup", event => updateKeyDown(event.key, "remove"));
+
+const updateState = ({ beat }) => {
+  const cursor = keys.role.sequence[beat % 16];
+  keys.role.sequence.forEach((key, index) => {
+    getKeyElement(key).classList.toggle("lit", index === beat % keys.role.sequence.length);
   });
-})();
+};
+
+if (window.groovebox)
+  (function mainLoop() {
+    requestAnimationFrame(mainLoop);
+    groovebox().then(updateState);
+  })();
