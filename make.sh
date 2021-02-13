ee0ba0f787990a1f7f5da0409b3d5bbb008a8754
Kofi Gumbs <h.kofigumbs@gmail.com>
2021-02-13 16:32:00 -0500

    Add build directory to git

--- make.sh
+++ make.sh
@@ -2,7 +2,6 @@
 
 set -ex
 
-mkdir -p build
 git submodule update --init --recursive
 faust -os -cn Effects -o build/Effects.h audio/Effects.dsp
 g++ desktop/main.cpp -std=c++17 -I build -I `faust --includedir` -ldl -lm -lpthread -framework WebKit -o build/groovebox
