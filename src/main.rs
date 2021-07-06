d58dff4dd46db57c9faeeff163e33107b9eebe28
Kofi Gumbs <h.kofigumbs@gmail.com>
2021-07-05 22:22:02 -0400

    Samples

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,75 @@
 extern crate anyhow;
+extern crate atomic_float;
+extern crate audrey;
 extern crate cpal;
 extern crate serde;
 
 use anyhow::{Context, Result};
+use atomic_float::AtomicF32;
+use audrey::read::Reader;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use cpal::{Sample, SampleRate};
+use cpal::SampleRate;
 use serde::Deserialize;
-use std::sync::atomic::{AtomicBool, AtomicI16, Ordering};
-use std::sync::mpsc::{channel, Receiver, Sender};
+use std::sync::atomic::{AtomicBool, AtomicI16, AtomicU8, Ordering};
+use std::sync::mpsc::{Receiver, Sender};
 use std::sync::Arc;
 use wry::application::event::{Event, WindowEvent};
 use wry::application::event_loop::{ControlFlow, EventLoop};
 use wry::application::window::WindowBuilder;
 use wry::webview::{RpcRequest, RpcResponse, WebViewBuilder};
 use wry::Value;
+
+const VOICE_COUNT: u8 = 5;
+const TRACK_COUNT: u8 = 15;
+
+struct SampleFile {
+    channels: u32,
+    samples: Vec<f32>,
+}
+
+impl SampleFile {
+    fn read(i: u8) -> Result<Self> {
+        let mut path = std::env::current_dir()?;
+        path.push("audio");
+        path.push("samples");
+        path.push(format!("{:02}.wav", i));
+        let mut reader = Reader::open(path)?;
+        let channels = reader.description().channel_count();
+        let sample_rate = reader.description().sample_rate();
+        std::assert!(channels == 1 || channels == 2);
+        std::assert_eq!(sample_rate, 44100);
+        let samples = reader
+            .samples()
+            .collect::<Result<Vec<f32>, audrey::read::FormatError>>()?;
+        Ok(Self { channels, samples })
+    }
+
+    fn sample(&self, position: usize, channel: usize) -> f32 {
+        if self.mono() {
+            self.samples[position]
+        } else {
+            self.samples[2 * position + channel]
+        }
+    }
+
+    fn duration(&self) -> usize {
+        self.samples.len() / self.channels as usize
+    }
+
+    fn mono(&self) -> bool {
+        self.channels == 1
+    }
+}
+
+struct Track {
+    sample_file: SampleFile,
+}
+
+struct Voice {
+    track_id: AtomicU8,
+    position: AtomicF32,
+    increment: AtomicF32,
+}
 
 struct State {
     active_track: AtomicI16,
@@ -22,18 +78,35 @@ struct State {
     tempo: AtomicI16,
     root: AtomicI16,
     scale: AtomicI16,
+    voices: Vec<Voice>,
+    tracks: Vec<Track>,
 }
 
 impl State {
-    fn new() -> Self {
-        State {
+    fn new() -> Result<Self> {
+        let mut state = State {
             active_track: AtomicI16::new(0),
             playing: AtomicBool::new(false),
             armed: AtomicBool::new(false),
             tempo: AtomicI16::new(120),
             root: AtomicI16::new(0),
             scale: AtomicI16::new(0),
+            tracks: Vec::with_capacity(TRACK_COUNT.into()),
+            voices: Vec::with_capacity(VOICE_COUNT.into()),
+        };
+        for i in 0..TRACK_COUNT {
+            state.tracks.push(Track {
+                sample_file: SampleFile::read(i)?,
+            });
         }
+        for _ in 0..VOICE_COUNT {
+            state.voices.push(Voice {
+                track_id: AtomicU8::new(0),
+                position: AtomicF32::new(0.0),
+                increment: AtomicF32::new(0.0),
+            });
+        }
+        Ok(state)
     }
 }
 
@@ -59,6 +132,7 @@ enum Getter {
 #[serde(rename_all = "camelCase")]
 enum Setter {
     ActiveTrack,
+    AuditionDown,
     Play,
     Arm,
     Tempo,
@@ -86,6 +160,59 @@ fn nudge(value: &AtomicI16, data: u8, jump: i16) {
     value.fetch_add(diff, Ordering::Relaxed);
 }
 
+fn key_down(state: &State, track_id: u8) {
+    let voice = &state.voices[0];
+    voice.track_id.store(track_id, Ordering::Relaxed);
+    voice.position.store(0.0, Ordering::Relaxed);
+    voice.increment.store(1.0, Ordering::Relaxed);
+}
+
+fn interpolate(x: f32, a: f32, b: f32) -> f32 {
+    a + x * (b - a)
+}
+
+fn on_audio(state: &State, receiver: &Receiver<(Setter, u8)>, audio: &mut [f32]) {
+    let messages = receiver.try_iter();
+    messages.for_each(|(setter, data)| match setter {
+        Setter::ActiveTrack => set(&state.active_track, data),
+        Setter::AuditionDown => key_down(&state, data),
+        Setter::Play => toggle(&state.playing),
+        Setter::Arm => toggle(&state.armed),
+        Setter::Tempo => nudge(&state.tempo, data, 10),
+        Setter::TempoTaps => set(&state.tempo, data),
+        Setter::Root => nudge(&state.root, data, 7),
+        Setter::Scale => set(&state.scale, data),
+    });
+    let mut iter = audio.iter_mut();
+    while let Some((l, r)) = iter.next().zip(iter.next()) {
+        *l = 0.0;
+        *r = 0.0;
+        for voice in state.voices.iter() {
+            let track = &state.tracks[voice.track_id.load(Ordering::Relaxed) as usize];
+            let position = voice.position.load(Ordering::Relaxed);
+            let position_i = position.floor() as usize;
+            let increment = voice.increment.load(Ordering::Relaxed);
+            if position_i < track.sample_file.duration() && position_i as f32 == position {
+                *l += track.sample_file.sample(position_i, 0);
+                *r += track.sample_file.sample(position_i, 1);
+                voice.position.fetch_add(increment, Ordering::Relaxed);
+            } else if position_i + 1 < track.sample_file.duration() {
+                *l += interpolate(
+                    position - position_i as f32,
+                    track.sample_file.sample(position_i, 0),
+                    track.sample_file.sample(position_i + 1, 0),
+                );
+                *r += interpolate(
+                    position - position_i as f32,
+                    track.sample_file.sample(position_i, 1),
+                    track.sample_file.sample(position_i + 1, 1),
+                );
+                voice.position.fetch_add(increment, Ordering::Relaxed);
+            }
+        }
+    }
+}
+
 fn get_state(state: &State, method: Getter) -> Value {
     match method {
         Getter::ActiveTrack => state.active_track.load(Ordering::Relaxed).into(),
@@ -94,27 +221,6 @@ fn get_state(state: &State, method: Getter) -> Value {
         Getter::Tempo => state.tempo.load(Ordering::Relaxed).into(),
         Getter::Root => state.root.load(Ordering::Relaxed).into(),
         Getter::Scale => state.scale.load(Ordering::Relaxed).into(),
-    }
-}
-
-fn set_state(state: &State, method: Setter, data: u8) {
-    match method {
-        Setter::ActiveTrack => set(&state.active_track, data),
-        Setter::Play => toggle(&state.playing),
-        Setter::Arm => toggle(&state.armed),
-        Setter::Tempo => nudge(&state.tempo, data, 10),
-        Setter::TempoTaps => set(&state.tempo, data),
-        Setter::Root => nudge(&state.root, data, 7),
-        Setter::Scale => set(&state.scale, data),
-    }
-}
-
-fn on_audio(state: &State, receiver: &Receiver<(Setter, u8)>, audio: &mut [f32]) {
-    for (message, data) in receiver.try_iter() {
-        set_state(state, message, data);
-    }
-    for sample in audio.iter_mut() {
-        *sample = Sample::from(&0.0);
     }
 }
 
@@ -141,9 +247,9 @@ fn main() -> Result<()> {
     std::assert_eq!(config.channels(), 2);
     std::assert_eq!(config.sample_rate(), SampleRate(44100));
 
-    let audio_state = Arc::new(State::new());
+    let (sender, receiver) = std::sync::mpsc::channel();
+    let audio_state = Arc::new(State::new()?);
     let ui_state = Arc::clone(&audio_state);
-    let (sender, receiver) = channel();
     let stream = device.build_output_stream(
         &config.into(),
         move |data, _| on_audio(&audio_state, &receiver, data),
