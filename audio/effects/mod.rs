3d5789550021947022168dddcca9e6c8ff214e29
Kofi Gumbs <h.kofigumbs@gmail.com>
2021-07-27 21:06:20 -0400

    Build faust

---
+++ audio/effects/mod.rs
@@ -0,0 +1,4 @@
+include!(concat!(env!("OUT_DIR"), "/insert.rs"));
+include!(concat!(env!("OUT_DIR"), "/reverb.rs"));
+include!(concat!(env!("OUT_DIR"), "/echo.rs"));
+include!(concat!(env!("OUT_DIR"), "/drive.rs"));
--- build.rs
+++ build.rs
@@ -1,24 +1,43 @@
 extern crate anyhow;
 
+use std::fs::DirEntry;
+use std::path::Path;
+use std::process::Command;
+
 use anyhow::Result;
 
+fn dsp_file(entry: DirEntry) -> Option<(String, String)> {
+    if entry.path().extension()? != "dsp" {
+        return None;
+    }
+    Some((
+        entry.path().to_string_lossy().into_owned(),
+        entry.path().file_stem()?.to_string_lossy().into_owned(),
+    ))
+}
+
 fn main() -> Result<()> {
-    // if !vendor/faust/build/bin/faust {
-    //     git submodule update --init --recursive
-    //     make -C vendor/faust
-    // }
-    // for dsp in audio/effects/*.dsp {
-    //     let output_path = env::var_os("OUT_DIR");
-    //     output_path.push(dsp);
-    //     let output = Command::new("vendor/faust/build/bin/faust")
-    //        .arg("-I")
-    //        .arg("vendor/faust/libraries")
-    //        .arg("-lang")
-    //        .arg("rust")
-    //        .arg("-o")
-    //        .arg(output_path.as_str())
-    //        .output()?;
-    //     println!("cargo:rerun-if-changed={}", dsp);
-    // }
+    if !Path::new("vendor/faust/build/bin/faust").exists() {
+        Command::new("git")
+            .args(&["submodule", "update", "--init", "--recursive"])
+            .status()?;
+        Command::new("make")
+            .args(&["-C", "vendor/faust"])
+            .status()?;
+    }
+    let out_dir = std::env::var("OUT_DIR")?;
+    let entries = Path::new("audio/effects").read_dir()?;
+    for (path, basename) in entries.flat_map(|x| dsp_file(x.ok()?)) {
+        let out = Path::new(&out_dir)
+            .join(&basename)
+            .with_extension("rs")
+            .to_string_lossy()
+            .into_owned();
+        Command::new("vendor/faust/build/bin/faust")
+            .args(&["-I", "vendor/faust/libraries"])
+            .args(&["-lang", "rust", "-cn", &basename, "-o", &out, &path])
+            .status()?;
+        println!("cargo:rerun-if-changed={}", &path);
+    }
     Ok(())
 }
