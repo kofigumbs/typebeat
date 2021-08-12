d90db7303e1be9286337c6fe4d3ab45458015fb5
Kofi Gumbs <h.kofigumbs@gmail.com>
2021-08-11 21:17:08 -0400

    Combine Player+Cursor => Voice, Rename Voices => Mixer

--- src/main.rs
+++ src/main.rs
@@ -112,26 +112,28 @@ impl Controls {
     }
 }
 
-#[derive(Default)]
-struct Cursor {
+struct Voice {
     age: usize,
     position: f32,
     increment: f32,
     track_id: Option<usize>,
+    insert: effects::insert,
 }
-impl Cursor {
+impl Voice {
     fn process(&mut self, track: &Track, input: &[f32], output: &mut [f32]) {
+        let mut pre_buffer = [0.; effects::insert::INPUTS];
+        let mut post_buffer = [0.; effects::insert::OUTPUTS];
         match track.sample_type.load() {
             SampleType::File => {
                 let duration = track.file_sample.duration();
                 let position = self.position.floor() as usize;
                 let position_rem = self.position.fract();
                 if position < duration && position_rem == 0. {
-                    self.advance(output, |channel, sample| {
+                    self.advance(&mut pre_buffer, |channel, sample| {
                         *sample += track.file_sample.at(position, channel);
                     });
                 } else if position + 1 < duration {
-                    self.advance(output, |channel, sample| {
+                    self.advance(&mut pre_buffer, |channel, sample| {
                         let a = track.file_sample.at(position, channel);
                         let b = track.file_sample.at(position + 1, channel);
                         *sample += a + position_rem * (b - a);
@@ -139,11 +141,17 @@ impl Cursor {
                 }
             }
             SampleType::Live | SampleType::LiveRecord | SampleType::LivePlay => {
-                for sample in output.iter_mut() {
+                for sample in pre_buffer.iter_mut() {
                     *sample += input.iter().sum::<f32>();
                 }
             }
         }
+        self.insert.compute(
+            1,
+            &pre_buffer.each_ref().map(std::slice::from_ref),
+            &mut post_buffer.each_mut().map(std::slice::from_mut),
+        );
+        add_assign_each(output, &post_buffer);
     }
     fn advance<F: Fn(usize, &mut f32)>(&mut self, frame: &mut [f32], write: F) {
         for (channel, sample) in frame.iter_mut().enumerate() {
@@ -153,56 +161,36 @@ impl Cursor {
     }
 }
 
-struct Player {
-    cursor: Cursor,
-    insert: effects::insert,
-}
-impl Player {
-    fn process(&mut self, track: &Track, input: &[f32], output: &mut [f32]) {
-        let mut pre_buffer = [0.; effects::insert::INPUTS];
-        let mut post_buffer = [0.; effects::insert::OUTPUTS];
-        self.cursor.process(track, input, &mut pre_buffer);
-        self.insert.compute(
-            1,
-            &pre_buffer.each_ref().map(std::slice::from_ref),
-            &mut post_buffer.each_mut().map(std::slice::from_mut),
-        );
-        add_assign_each(output, &post_buffer);
-    }
-}
-
-struct Voices {
-    players: Vec<Player>,
+struct Mixer {
+    voices: Vec<Voice>,
     sends: Vec<Box<dyn Send + FaustDsp<T = f32>>>,
 }
-impl Voices {
+impl Mixer {
     fn key_down(&mut self, track_id: i32) {
-        for player in self.players.iter_mut() {
-            player.cursor.age += 1;
+        for voice in self.voices.iter_mut() {
+            voice.age += 1;
         }
-        let mut player = self
-            .players
+        let mut voice = self
+            .voices
             .iter_mut()
-            .max_by_key(|player| player.cursor.age)
+            .max_by_key(|voice| voice.age)
             .unwrap();
-        player.cursor = Cursor {
-            age: 0,
-            position: 0.0,
-            increment: 1.0,
-            track_id: Some(track_id as usize),
-        };
-        player.insert.instance_clear();
+        voice.age = 0;
+        voice.position = 0.0;
+        voice.increment = 1.0;
+        voice.track_id = Some(track_id as usize);
+        voice.insert.instance_clear();
     }
     fn process(&mut self, controls: &Controls, input: &[f32], output: &mut [f32]) {
         let mut pre_buffer = [0.; effects::insert::OUTPUTS];
         let mut post_buffer = [0.; 2];
-        for player in self.players.iter_mut() {
-            let track = match player.cursor.track_id {
+        for voice in self.voices.iter_mut() {
+            let track = match voice.track_id {
                 None => continue,
                 Some(track_id) => &controls.tracks[track_id],
             };
-            track.effect_ui.set_params_on(&mut player.insert);
-            player.process(track, input, &mut pre_buffer);
+            track.effect_ui.set_params_on(&mut voice.insert);
+            voice.process(track, input, &mut pre_buffer);
             add_assign_each(output, &pre_buffer);
         }
         for (i, (dsp, ui)) in self.sends.iter_mut().zip(controls.sends.iter()).enumerate() {
@@ -225,7 +213,7 @@ enum Setter {
 
 struct Audio {
     controls: Arc<Controls>,
-    voices: Voices,
+    mixer: Mixer,
     receiver: Receiver<(Setter, i32)>,
 }
 impl Audio {
@@ -250,7 +238,7 @@ impl Audio {
             }
         }
         for (input, output) in input.frames::<f32>().zip(output.frames_mut::<f32>()) {
-            self.voices.process(&self.controls, input, output);
+            self.mixer.process(&self.controls, input, output);
         }
     }
 }
@@ -300,7 +288,7 @@ impl Ui {
             "play" => Setter::Toggle(|audio| &audio.controls.playing),
             "arm" => Setter::Toggle(|audio| &audio.controls.armed),
             "activeTrack" => Setter::Number(|audio, i| audio.controls.active_track_id.store(i)),
-            "auditionDown" => Setter::Number(|audio, i| audio.voices.key_down(i)),
+            "auditionDown" => Setter::Number(|audio, i| audio.mixer.key_down(i)),
             "tempo" => Setter::Number(|audio, i| audio.controls.tempo.nudge(i, 10)),
             "tempoTaps" => Setter::Number(|audio, i| audio.controls.tempo.store(i)),
             "root" => Setter::Number(|audio, i| audio.controls.root.nudge(i, 7)),
@@ -330,46 +318,49 @@ fn main() -> Result<()> {
         });
     }
 
-    let mut players = Vec::new();
-    players.resize_with(VOICE_COUNT as usize, || Player {
-        cursor: Cursor::default(),
+    let mut voices = Vec::new();
+    voices.resize_with(VOICE_COUNT as usize, || Voice {
+        age: 0,
+        position: 0.,
+        increment: 0.,
+        track_id: None,
         insert: new_dsp::<effects::insert>(),
     });
 
-    let voices = Voices {
-        players,
+    let mixer = Mixer {
+        voices,
         sends: vec![
             Box::new(new_dsp::<effects::reverb>()),
             Box::new(new_dsp::<effects::echo>()),
             Box::new(new_dsp::<effects::drive>()),
         ],
     };
-    for dsp in voices.sends.iter() {
+    for dsp in mixer.sends.iter() {
         let mut ui = EffectUi::default();
         dsp.build_user_interface(&mut ui);
         controls.sends.push(ui);
     }
-    for player in voices.players.iter() {
-        assert_eq!(player.insert.get_num_inputs(), 2);
+    for voice in mixer.voices.iter() {
+        assert_eq!(voice.insert.get_num_inputs(), 2);
         assert_eq!(
-            voices.sends.len() + 1,
-            player.insert.get_num_outputs() as usize / 2
+            mixer.sends.len() + 1,
+            voice.insert.get_num_outputs() as usize / 2
         );
     }
-    for dsp in voices.sends.iter() {
+    for dsp in mixer.sends.iter() {
         assert_eq!(dsp.get_num_inputs(), 2);
         assert_eq!(dsp.get_num_outputs(), 2);
     }
 
     let (sender, receiver) = std::sync::mpsc::channel();
     let ui = Ui {
-        sender,
         controls: Arc::new(controls),
+        sender,
     };
     let mut audio = Audio {
-        receiver,
         controls: Arc::clone(&ui.controls),
-        voices,
+        mixer,
+        receiver,
     };
 
     let mut device_config = DeviceConfig::new(DeviceType::Duplex);
