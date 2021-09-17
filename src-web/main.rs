4d8dbb5498f6f3b96ffc136f39efbecdb0551193
Kofi Gumbs <h.kofigumbs@gmail.com>
2021-09-17 01:43:26 -0400

    Send strings to Rust via CStr

--- src-web/main.rs
+++ src-web/main.rs
@@ -1,3 +1,6 @@
+use std::ffi::CStr;
+use std::os::raw::c_char;
+
 use typebeat::{Controller, Platform};
 
 struct StaticPlatform;
@@ -8,14 +11,24 @@ impl Platform for StaticPlatform {
     }
 }
 
-#[no_mangle]
-pub fn set(controller: *const Controller, method: &str, data: i32) {
-    unsafe { &*controller }.set(method, data)
+fn c_str(chars: *const c_char) -> Option<&'static str> {
+    unsafe { chars.as_ref().map(|chars| CStr::from_ptr(chars)) }
+        .and_then(|chars| chars.to_str().ok())
 }
 
 #[no_mangle]
-pub fn get(controller: *const Controller, method: &str) -> i32 {
-    unsafe { &*controller }.get(method).unwrap_or_default()
+pub fn set(controller: *const Controller, method: *const c_char, data: i32) {
+    unsafe { controller.as_ref() }
+        .zip(c_str(method))
+        .map(|(controller, method)| controller.set(method, data));
+}
+
+#[no_mangle]
+pub fn get(controller: *const Controller, method: *const c_char) -> i32 {
+    unsafe { controller.as_ref() }
+        .zip(c_str(method))
+        .and_then(|(controller, method)| controller.get(method))
+        .unwrap_or_default()
 }
 
 #[no_mangle]
