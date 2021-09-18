5c6c43f1196031d628e12f6d5730abd6ed13c2de
Kofi Gumbs <h.kofigumbs@gmail.com>
2021-09-18 18:46:37 -0400

    Refactor front-end to use workspaces/modules

--- src-ui/index.js
+++ src-ui/index.js
@@ -1,4 +1,44 @@
-window.Bindings = ({ state, set }) => {
+import './FiraCode/distr/fira_code.css';
+import './index.css';
+import './Themes/scripts/lib/theme.js';
+import './tare/dist/tare.min.js';
+import './typed-label.js';
+
+
+/*
+ * state (global)
+ */
+
+const State = ({ get }) => {
+  const cache = new Map();
+  const local = new Map([
+    ['invalidate', cache.clear.bind(cache)],
+    ['modifier', undefined],
+    ['tempoTaps', []],
+  ]);
+  return new Proxy({}, {
+    get: function(target, prop) {
+      if (local.has(prop))
+        return local.get(prop);
+      else if (cache.has(prop))
+        return cache.get(prop)
+      const value = get(prop);
+      cache.set(prop, value);
+      return value;
+    },
+    set: function(target, prop, value) {
+      local.set(prop, value);
+      return true;
+    },
+  });
+};
+
+
+/*
+ * bindings (global)
+ */
+
+const Bindings = ({ set }) => {
   // join two method name parts with camel case
   const join = (a, b) => `${a}${b[0].toUpperCase()}${b.substring(1)}`.replace(/[^\w]/, '');
 
@@ -169,4 +209,140 @@ window.Bindings = ({ state, set }) => {
       })),
     ])}],
   ]);
+};
+
+
+/*
+ * sync
+ */
+
+const forceClass = (el, className) => {
+  el.classList.remove(className);
+  void el.offsetWidth; // trigger a DOM reflow
+  el.classList.add(className);
+}
+
+const sync = async () => {
+  state.invalidate();
+  const binding = bindings.get(state.modifier);
+  for (let i = 0; i < keysOnRight.length; i++) {
+    const key = keysOnRight[i];
+    const action = binding.actions.get(key.dataset.cap);
+    labels[i].ariaLabel = await action?.label() ?? '';
+    if (!!state.modifier)
+      labels[i].classList.toggle('title', !!(await action?.title()));
+    if (await state[`recent ${i}`])
+      forceClass(minipads[i], 'pulse');
+    minipads[i].classList.toggle('active', i === await state.activeTrack);
+  };
+  if (await state.playing)
+    requestSync();
+};
+
+let nextSyncId;
+const clearNextSyncIdAndSync = () => {
+  nextSyncId = null;
+  sync();
+};
+const requestSync = () => {
+  if (!nextSyncId)
+    nextSyncId = requestAnimationFrame(clearNextSyncIdAndSync);
+};
+
+
+/*
+ * events
+ */
+
+const hasModifier = event => (
+  event.ctrlKey || event.metaKey || event.shiftKey || event.altKey
+);
+
+const capsByEventCode = new Map([
+  ['Semicolon', ';'], ['Comma', ','], ['Period', '.'], ['Slash', '/'],
+  ...Array.from('QWERTYUIOPASDFGHJKLZXCVBNM', cap => [`Key${cap}`, cap]),
+]);
+
+const handleDocumentKey = event => {
+  if (hasModifier(event))
+    return;
+  event.preventDefault();
+  if (event.repeat)
+    return;
+  const cap = capsByEventCode.get(event.code);
+  if (!cap)
+    return;
+  const down = event.type === 'keydown';
+  const binding = bindings.has(cap);
+  if (binding && !down)
+    return;
+  if (binding) {
+    state.modifier = state.modifier === cap ? undefined : cap;
+    for (const key of keysOnLeft)
+      key.classList.toggle('mode', !!state.modifier && key.dataset.cap === state.modifier);
+    state.tempoTaps = [];
+  }
+  else {
+    const handler = bindings.get(state.modifier).actions.get(cap);
+    down ? handler?.onDown(event.timeStamp) : handler?.onUp(event.timeStamp);
+    if (down)
+      forceClass(keysOnRight.find(key => cap === key.dataset.cap), 'pulse');
+  }
+  requestSync();
+};
+
+export default (callback) => {
+  const theme = new window.Theme();
+  theme.install();
+  theme.start();
+  theme.onLoad = () => {
+    for (const [name, value] of Object.entries(theme.active))
+      document.documentElement.style.setProperty(`--${name}`, value);
+  };
+
+  globalThis.state = State({ get: method => callback('get', { method }) });
+  globalThis.bindings = Bindings({ set: (method, data) => callback('set', { method, data }) });
+
+  const capsOnLeft = 'ZXCVBASDFGQWERT';
+  const capsOnRight = 'NM,./HJKL;YUIOP';
+  const mapJoin = (iterable, f) => Array.from(iterable).map(f).join('');
+  document.querySelector('.mount').innerHTML += mapJoin(['QWERTYUIOP', 'ASDFGHJKL;', 'ZXCVBNM,./'], row => `
+    <div class="row">
+      ${mapJoin(row, cap => {
+        if (cap === 'Q')
+          return `
+            <div class="key" data-cap="${cap}">
+              ${mapJoin(capsOnRight.match(/.{1,5}/g).reverse(), minirow => `
+                <div class="minirow">
+                  ${mapJoin(minirow, c => `<div class="minipad" data-cap="${c}"></div>`)}
+                </div>`
+              )}
+            </div>
+          `;
+        else if (bindings.has(cap))
+          return `
+            <div class="key" data-cap="${cap}">
+              ${Tare.html(bindings.get(cap).mode)}
+            </div>
+          `;
+        else
+          return `
+            <div class="key pad" data-cap="${cap}">
+              <typed-label class="label" aria-label=""></typed-label>
+            </div>
+          `;
+      })}
+    </div>
+  `);
+
+  const findElements = (caps, f) => Array.from(caps).map(cap => document.querySelector(f(cap)));
+  globalThis.keysOnLeft = findElements(capsOnLeft, cap => `.key[data-cap="${cap}"]`);
+  globalThis.keysOnRight = findElements(capsOnRight, cap => `.key[data-cap="${cap}"]`);
+  globalThis.labels = findElements(capsOnRight, cap => `.key[data-cap="${cap}"] .label`);
+  globalThis.minipads = findElements(capsOnRight, cap => `.minipad[data-cap="${cap}"]`);
+
+  document.addEventListener('keydown', handleDocumentKey);
+  document.addEventListener('keyup', handleDocumentKey);
+  document.addEventListener('keypress', event => !hasModifier(event));
+  sync();
 };
