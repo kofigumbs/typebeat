f7fa9daf2f9e383904e8208e137c7ed95b3644c0
Kofi Gumbs <h.kofigumbs@gmail.com>
2021-09-23 18:30:39 -0400

    Cosmetic

--- src-web/client/vite.config.js
+++ src-web/client/vite.config.js
@@ -1,7 +1,7 @@
 /* 
- * Parse guide markdown for content and checks (encoded like `key=value`)
+ * Parse GUIDE.md for content and checks (encoded as `key=value`)
  */
-const guide = () => {
+const transformGuide = () => {
   const horizontalRules = /(?:\n+)---(?:\n+)/;
   const trailingNewlinesAndCodeFenceChecks = /\n+(?:`.+\n*)$/;
   const paragraphBreaks = /\n(?:\n+)/;
@@ -11,17 +11,16 @@ const guide = () => {
     transform(src, path) {
       if (!path.endsWith('/GUIDE.md'))
         return;
-      const sections = src.split(horizontalRules).map(markup => {
-        const content = markup
+      const sections = src.split(horizontalRules).map(markup => ({
+        content: markup
           .replace(trailingNewlinesAndCodeFenceChecks, '')
           .split(paragraphBreaks)
           .map(x => x.startsWith('<') ? x : `<p>${x}</p>`)
-          .join('');
-        const checks = [];
-        for (let match of markup.matchAll(codeFenceChecks))
-          checks.push([match[1], match[2]]);
-        return { content, checks };
-      });
+          .join(''),
+        checks: Array.from(markup.matchAll(codeFenceChecks), match => (
+          [match[1], match[2]]
+        )),
+      }));
       return {
         code: `export default ${JSON.stringify(sections)}`,
         map: null,
@@ -31,7 +30,7 @@ const guide = () => {
 };
 
 export default {
-  plugins: [guide()],
+  plugins: [transformGuide()],
   hmr: false,
   build: { sourcemap: true },
 };
