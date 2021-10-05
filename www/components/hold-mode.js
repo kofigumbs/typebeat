bea0567e7cfeef31375256abc924def3d1e233b1
Kofi Gumbs <h.kofigumbs@gmail.com>
2021-10-05 18:49:07 -0400

    Visuals for hold and note

--- www/components/hold-mode.js
+++ www/components/hold-mode.js
@@ -7,3 +7,32 @@ export const actions = (local, proxy, set) => new Map([
   ...bind.nudge(async () => await proxy[local.hold], i => set(local.hold, i)),
   ['N', bind.toggle('sample', async () => await proxy.holdSample, () => set('holdSample')) ],
 ]);
+
+customElements.define('hold-mode', class extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg">
+        <style>
+          hold-mode path {
+            stroke: var(--dark);
+          }
+        </style>
+        <path id="hold" fill="none" stroke-width="2"></path>
+      </svg>
+    `;
+    this._path = this.querySelector('#hold');
+  }
+
+  async sync({ proxy }) {
+    const a = await proxy.attack;
+    const d = await proxy.decay;
+    const s = await proxy.sustain;
+    const r = await proxy.release;
+    this._path.setAttribute('d', `
+      M 8 40 l ${a*20/50} -34
+      l ${d*20/50} ${34 * (1-s/50)}
+      H ${88 - r*20/50}
+      L 88 40
+    `);
+  }
+});
--- www/components/note-mode.js
+++ www/components/note-mode.js
@@ -10,3 +10,45 @@ export const actions = (local, proxy, set) => new Map([
     onUp: () => set('noteUp', i),
   })),
 ]);
+
+customElements.define('note-mode', class extends HTMLElement {
+  // DOM ids in z order (white keys then black keys)
+  ids = [
+    "note-0", "note-2", "note-4", "note-5", "note-7", "note-9", "note-11",
+    "note-1", "note-3",           "note-6", "note-8", "note-10",
+  ];
+
+  connectedCallback() {
+    const whiteWidth = 14;
+    const whiteHeight = 48;
+    const blackWidth = 10;
+    const key = (x, w, h) => `
+      <rect id="${this.ids.shift()}" x="${x}" height="${h}" width="${w}" fill="none"></rect>
+    `;
+    const white = i => key(i*whiteWidth, whiteWidth, whiteHeight);
+    const black = i => key((i+1)*whiteWidth - blackWidth/2, blackWidth, whiteHeight/2);
+    this.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg">
+        <style>
+          note-mode rect {
+            stroke: var(--dark);
+            fill: var(--key_background);
+            transform: translate(-1px, -1px);
+          }
+          note-mode rect.active {
+            fill: var(--secondary);
+          }
+        </style>
+        ${Array.from({ length: 7 }).map((_, i) => white(i)).join('')}
+        ${Array.from({ length: 2 }).map((_, i) => black(i)).join('')}
+        ${Array.from({ length: 3 }).map((_, i) => black(i + 3)).join('')}
+      </svg>
+    `;
+    this._notes = Array.from({ length: 12 }).map((_, i) => this.querySelector(`#note-${i}`));
+  }
+
+  async sync({ proxy }) {
+    const activeNote = await proxy[`note ${await proxy.activeKey}`] % 12;
+    this._notes.forEach((note, i) => note.classList.toggle('active', i === activeNote));
+  }
+});
