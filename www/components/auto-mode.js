d52faf1beabfcad6add14d893ce9009ccd69e5c2
Kofi Gumbs <h.kofigumbs@gmail.com>
2021-10-10 00:03:25 -0400

    Auto mode visual

--- www/components/auto-mode.js
+++ www/components/auto-mode.js
@@ -3,3 +3,14 @@ import bind from '../bind';
 export const cap = 'X';
 
 export const actions = (local, proxy, set) => bind.comingSoon;
+
+customElements.define('auto-mode', class extends HTMLElement {
+  connectedCallback() {
+    const ls = Array.from({ length: 50 }).map((_, i, a) => `l 1 ${Math.cos(i/a.length*2*Math.PI)}`);
+    this.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg">
+        <path d="M 23 23 ${ls.join('')}" stroke-width="2" fill="none"></path>
+      </svg>
+    `;
+  }
+});
