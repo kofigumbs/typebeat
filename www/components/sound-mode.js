fd0723a5b733d126e224ad5b814cd5903eefe115
Kofi Gumbs <h.kofigumbs@gmail.com>
2021-10-09 22:56:05 -0400

    Sound mode visual

--- www/components/sound-mode.js
+++ www/components/sound-mode.js
@@ -26,3 +26,43 @@ export const actions = (local, proxy, set) => new Map([
     };
   }),
 ]);
+
+const length = 12;
+const noise = Array.from({ length }).map(() => Math.random()*2 - 1);
+
+customElements.define('sound-mode', class extends HTMLElement {
+  synths = [
+    (i, n) => Math.sin(i/n*2*Math.PI),
+    (i) => [0, 1, 0, -1][i % 4],
+    (i) => [-1, 0, 1][i % 3],
+    (i) => [1, 1, -1, -1][i % 4],
+    (i) => noise[i],
+  ];
+
+  connectedCallback() {
+    this.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg">
+        ${Array.from({ length }).map((_, i) => `<path stroke-width="2"></path>`).join('')}
+      </svg>
+    `;
+    this._paths = Array.from(this.querySelectorAll('path'));
+  }
+
+  async sync({ proxy }) {
+    const amplitude = 20 * await proxy.sampleLevel / 50 + 4;
+    const synth1 = this.synths[await proxy.synth1Type];
+    const synth2 = this.synths[await proxy.synth2Type];
+    const synth3 = this.synths[await proxy.synth3Type];
+    const offset1 = await proxy.synth1Level/50 * 12
+    const offset2 = await proxy.synth2Level/50 * 12
+    const offset3 = await proxy.synth3Level/50 * 12
+    for (let i = 0; i < length; i++) {
+      const x = 48 - 4*(i-length/2);
+      const y = 23 - amplitude/2 +
+        synth1(i, length) * offset1 +
+        synth2(i, length) * offset2 +
+        synth3(i, length) * offset3;
+      this._paths[i].setAttribute('d', `M ${x} ${y} v ${amplitude}`);
+    }
+  }
+});
