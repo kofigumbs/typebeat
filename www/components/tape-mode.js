b5aebc0d18204d16bc3d645ce23f30ae7b77444c
Kofi Gumbs <h.kofigumbs@gmail.com>
2021-10-09 21:56:58 -0400

    Make tape less noisy

--- www/components/tape-mode.js
+++ www/components/tape-mode.js
@@ -6,19 +6,10 @@ export const actions = (local, proxy, set) => bind.comingSoon;
 
 customElements.define('tape-mode', class extends HTMLElement {
   connectedCallback() {
-    const y = 23;
-    const circles = [{ x: 23,  r: 16 }, { x: 23,  r: 8 }, { x: 73, r: 24 }, { x: 73, r: 8 }];
     this.innerHTML = `
       <svg xmlns="http://www.w3.org/2000/svg">
-        <style>
-          tape-mode.visual circle:nth-of-type(odd) {
-            fill: var(--dark);
-          }
-          tape-mode.visual circle:nth-of-type(even) {
-            fill: var(--key_background);
-          }
-        </style>
-        ${circles.map(({ x, r, cls }) => `<circle cx="${x}" cy="${y}" r="${r}"></circle>`).join('')}
+        <circle cx="73" cy="23" r="24" fill="none" stroke-width="2"></circle>
+        <circle cx="23" cy="23" r="24" fill="none" stroke-width="2"></circle>
       </svg>
     `;
   }
