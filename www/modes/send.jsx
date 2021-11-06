38a01b2535a07c31dd5906e7dcc938e821e36c7b
Kofi Gumbs <h.kofigumbs@gmail.com>
2021-11-06 17:34:38 -0400

    Send visual

--- www/modes/send.jsx
+++ www/modes/send.jsx
@@ -1,3 +1,5 @@
+import { createMemo } from 'solid-js';
+
 import Actions from '../actions';
 
 export const cap = 'C';
@@ -12,4 +14,29 @@ export const actions = Actions.tabbed(
   { cap: 'Y', label: 'reverb', actions: subtabs('reverb') },
   { cap: 'U', label: 'echo',   actions: subtabs('echo') },
   { cap: 'I', label: 'drive',  actions: subtabs('drive') }
+);
+
+const Fader = props => {
+  const margin = 3;
+  const x = (props.i+1) * 24;
+  const y = createMemo(() => {
+    const gain = props.state.song[`${props.effect}Gain`];
+    const feed = props.state.song[`${props.effect}Feed`];
+    const space = props.state.song[`${props.effect}Space`];
+    return (1 - gain/50*(feed/100 + space/100)) * 40;
+  });
+  return (
+    <>
+      <path d={`M ${x} ${margin} v 40`} stroke-width='2' />
+      <path d={`M ${x-6} ${y() + margin} h 12`} stroke-width='2' />
+    </>
+  );
+};
+
+export const Visual = props => (
+  <svg xmlns="http://www.w3.org/2000/svg">
+    <For each={['reverb', 'echo', 'drive']}>
+      {(effect, i) => <Fader effect={effect} i={i()} {...props} />}
+    </For>
+  </svg>
 );
