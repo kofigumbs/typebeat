a858863059956fdc5d212a3ce067f641a60de927
Kofi Gumbs <h.kofigumbs@gmail.com>
2021-12-07 21:47:17 -0500

    Fix macOS build

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
 fn main() {
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "macos" {
+        println!("cargo:rustc-link-lib=framework=ColorSync");
+    }
     #[cfg(not(target_arch = "wasm32"))]
     tauri_build::build();
 }
