ceb86f6e941849b5bfb47a06eb2fd8a59f77ee81
Kofi Gumbs <h.kofigumbs@gmail.com>
2022-01-09 18:48:29 -0500

    Fix icons path for windows build

--- build.rs
+++ build.rs
@@ -1,7 +1,14 @@
+use tauri_build::{Attributes, WindowsAttributes};
+
 fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "macos" {
         println!("cargo:rustc-link-lib=framework=ColorSync");
     }
     #[cfg(not(target_arch = "wasm32"))]
-    tauri_build::build();
+    if let Err(error) = tauri_build::try_build(
+        Attributes::new()
+            .windows_attributes(WindowsAttributes::new().window_icon_path("assets/icons/icon.ico")),
+    ) {
+        panic!("error found during tauri-build: {}", error);
+    }
 }
