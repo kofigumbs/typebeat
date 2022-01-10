1fbc77ee58045cfa21bebd4d1cc1a0cdae950044
Kofi Gumbs <h.kofigumbs@gmail.com>
2022-01-09 23:19:22 -0500

    Add platform check for tauri import

--- build.rs
+++ build.rs
@@ -1,14 +1,11 @@
-use tauri_build::{Attributes, WindowsAttributes};
-
 fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "macos" {
         println!("cargo:rustc-link-lib=framework=ColorSync");
     }
     #[cfg(not(target_arch = "wasm32"))]
-    if let Err(error) = tauri_build::try_build(
-        Attributes::new()
-            .windows_attributes(WindowsAttributes::new().window_icon_path("assets/icons/icon.ico")),
-    ) {
+    if let Err(error) = tauri_build::try_build(tauri_build::Attributes::new().windows_attributes(
+        tauri_build::WindowsAttributes::new().window_icon_path("assets/icons/icon.ico"),
+    )) {
         panic!("error found during tauri-build: {}", error);
     }
 }
